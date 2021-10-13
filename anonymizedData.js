e3e77ec4d102e2da1b6e9453c6c889d6567675c8
martiplaceres <martinaplaceres@hotmail.com>
2021-10-13 10:41:48 +0200

    Add files with array and map exercises

---
+++ anonymizedData.js
@@ -0,0 +1,17 @@
+const patients = require("./patients");
+console.log(patients);
+
+// map executes a callback for each element in the array
+const anonymizedData = patients.map(function(patient) {
+  // return an object with id and age, it gets added to the output array
+  return {
+      id: patient.id,
+      gender: patient.gender,
+      height: patient.height,
+      weight: patient.weight,
+      dailyExercise: patient.dailyExercise,
+      age: patient.age
+  };
+});
+
+console.log(anonymizedData);
